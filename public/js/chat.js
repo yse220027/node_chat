35afeea133a01c41cb4132855d1ac0ea96b9889b
yse220027 <220027gs@yse-c.net>
2023-12-13 11:24:18 +0900

    update

---
+++ public/js/chat.js
@@ -0,0 +1,7 @@
+
+
+function sendMessage() {
+    // console.log('send message!!')
+    var message = document.getElementById('message').value;
+    console.log(message);
+}
--- server.js
+++ server.js
@@ -10,11 +10,33 @@ app.use(express.static(__dirname + '/public'));
 const { createServer } = require('node:http');
 const server = createServer(app);
 
+
 //環境変数読み込み
 const dotenv = require('dotenv');
 dotenv.config();
 const host = process.env.HOST;
 const port = process.env.PORT;
+
+// Socket.ioで通信
+// Socket.io モジュール読み込み
+const { Server } = require('socket.io');
+// Socket.io 作成
+const io = new Server(server);
+
+// connection event
+io.on('connection', (socket) => {
+    console.log('connected!!!')
+
+    //chatメッセージの受信
+    socket.on('chat_message', (data) => {
+        console.log(socket.id);
+        console.log(data);
+        //送信ユーザのSocketIDを追加
+        data.socket_id = socket.id;
+        //接続しているユーザにメッセージを送信
+        io.emit('chat_message', data);
+    })
+})
 
 // HTTPサーバー待機
 server.listen(port, host, ()=> {
