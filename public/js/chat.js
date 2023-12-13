841370cc4bf8a3759abe2f03aee5ac5c7f997593
yse220027 <220027gs@yse-c.net>
2023-12-13 11:51:20 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,23 @@
+// Chatサーバに接続
+const URL = "";
+const socket = io.connect(URL); 
 
+socket.on('chat_message', (data) => {
+    console.log(data);
+    var p = document.createElement('p');
+    p.innerText = data.socket_id + ": " + data.message;
+    document.getElementById('chat-list').prepend(p);
+})
 
 function sendMessage() {
     // console.log('send message!!')
     var message = document.getElementById('message').value;
     console.log(message);
+    if (message) {
+        //データを作成
+        var data = { 
+            message: message
+        }
+        socket.emit('chat_message', data);
+    }
 }
--- server.js
+++ server.js
@@ -26,13 +26,13 @@ const io = new Server(server);
 // connection event
 io.on('connection', (socket) => {
     console.log('connected!!!')
+    console.log(`SocketID: ${socket.id}`);
 
     //chatメッセージの受信
     socket.on('chat_message', (data) => {
-        console.log(socket.id);
         console.log(data);
         //送信ユーザのSocketIDを追加
-        data.socket_id = socket.id;
+        data.socketID = socket.id;
         //接続しているユーザにメッセージを送信
         io.emit('chat_message', data);
     })
